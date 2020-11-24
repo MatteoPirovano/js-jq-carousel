80c6f4b474f1109a724275cbd371c6205a56cecf
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-11-24 17:33:08 +0100

    Terzo commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(
 
     $('.prev').click(
       function() {
-
+        scrollLeft();
       }
     )
     $('.next').click(
@@ -14,9 +14,39 @@ $(document).ready(
 
   }
 );
-function scrollLeft () {}
+// Scorriento verso sinistra-----------------------------------
+function scrollLeft () {
+  var imageSelection = $('img.active ');
+  imageSelection.removeClass('active');
+  var circleSelection = $('i.active')
+  circleSelection.removeClass('active')
+
+
+  var imageFirst = $('img.first');
+  var circleFirst = $('i.first')
+  if (imageFirst == true) {
+    imageLast.addClass('active');
+    circleLast.addClass('active');
+  } else {
+    imageSelection.prev().addClass('active');
+    circleSelection.prev().addClass('active')
+  }
+}
+
+// Scorriento verso destra-----------------------------------
 function scrollRight () {
   var imageSelection = $('img.active ');
   imageSelection.removeClass('active');
-  imageSelection.next().addClass('active');
+  var circleSelection = $('i.active')
+  circleSelection.removeClass('active')
+
+  var imageLast = $('img.last');
+  var circleLast = $('i.last')
+  if (imageLast == true && circleLast == true) {
+    imageFirst.addClass('active');
+    circleFirst.addClass('active');
+  } else {
+    imageSelection.next().addClass('active');
+    circleSelection.next().addClass('active')
+  }
 }
